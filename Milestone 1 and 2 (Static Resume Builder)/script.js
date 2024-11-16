d3917e8137b94aaf4f7b568cd307f9ae3314e206
AzmeenaAbdulJabbbar <56413566+AzmeenaAbdulJabbbar@users.noreply.github.com>
2024-11-16 16:14:20 +0500

    Resume Builder

--- Milestone 1 and 2 (Static Resume Builder)/script.js
+++ Milestone 1 and 2 (Static Resume Builder)/script.js
@@ -1,12 +1,13 @@
 var toggleButton = document.getElementById('toggle-skills');
 var skills = document.getElementById('skills');
+skills.style.display = 'block';
 toggleButton.addEventListener('click', function () {
     if (skills.style.display === 'none') {
-        skills.style.display = 'block';
-        toggleButton.textContent = 'Hide Skills'; // Updated text content
+        skills.style.display = 'block'; // Show skills
+        toggleButton.textContent = 'Hide Skills'; // This is the update button section
     }
     else {
-        skills.style.display = 'none';
-        toggleButton.textContent = 'Show Skills'; // Updated text content
+        skills.style.display = 'none'; // Hide skills
+        toggleButton.textContent = 'Show Skills'; // Update button text
     }
 });
